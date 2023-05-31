44f0df161a212713ab8e4265fd0b2b7c27aab72d
Lucas Brito <lucasbrito1@hotmail.com>
2023-05-31 11:41:00 -0300

    chore: changed size loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { RefreshCw } from "lucide-react";
 export default function Loading() {
   return (
     <div className="flex h-full items-center justify-center gap-[0.625rem] font-[700] text-gray">
-      <RefreshCw className="animate-spin" />
+      <RefreshCw className="animate-spin" size={16} />
       Carregando dados...
     </div>
   );
